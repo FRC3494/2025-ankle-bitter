378c1142c88e1cdb6aca6a0544721ec3e0c8bc4a
liujip0 <liujip2020@gmail.com>
2025-10-06 21:01:12 -0400

    use original template robotcontainer

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-ankle-bitter";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2025-10-06 20:37:38 EDT";
-  public static final long BUILD_UNIX_TIME = 1759797458904L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "13fe9617f56e1fd6afd4019f845e12d575dea3b2";
+  public static final String GIT_DATE = "2025-10-06 20:49:05 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-10-06 20:55:22 EDT";
+  public static final long BUILD_UNIX_TIME = 1759798522000L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
