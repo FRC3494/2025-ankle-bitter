8a1d3344f11e5577364ce5919d27d6ec23c037aa
liujip0 <liujip2020@gmail.com>
2025-11-13 19:55:11 -0500

    buildiconstants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-ankle-bitter";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 14;
-  public static final String GIT_SHA = "5d77af23bd5054b355ed4f6fe18e36de893ce540";
-  public static final String GIT_DATE = "2025-11-05 20:17:47 EST";
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "8fc2ff603006c97ec8f268820b8a7405bed6e016";
+  public static final String GIT_DATE = "2025-11-08 12:36:54 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-11-07 19:15:38 EST";
-  public static final long BUILD_UNIX_TIME = 1762560938861L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-11-13 19:49:34 EST";
+  public static final long BUILD_UNIX_TIME = 1763081374459L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
