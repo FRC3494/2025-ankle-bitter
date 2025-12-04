5e174c6ea63663fffbb73cf5a6adac9199cf85c7
liujip0 <liujip2020@gmail.com>
2025-12-04 18:29:16 -0500

    Revert "tune turn motor pid"
    This reverts commit 407d3aa988299d5ef2088c20cbf2a9583a7edbb0.

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -107,8 +107,8 @@ public class DriveConstants {
   public static final double turnEncoderVelocityFactor = (2 * Math.PI) / 60.0; // RPM -> Rad/Sec
 
   // Turn PID configuration
-  public static final double turnKp = 1.3;
-  public static final double turnKd = 1.0;
+  public static final double turnKp = 0.5;
+  public static final double turnKd = 0.0;
   public static final double turnSimP = 8.0;
   public static final double turnSimD = 0.0;
   public static final double turnPIDMinInput = 0; // Radians
