0aa87d7fb3614b61941542a7359e673e5b0ce6f6
liujip0 <liujip2020@gmail.com>
2025-12-23 11:15:51 -0500

    add trajectory logging for autos

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -3,9 +3,12 @@ package frc.robot;
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
+import choreo.trajectory.SwerveSample;
+import choreo.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.drive.Drive;
+import org.littletonrobotics.junction.Logger;
 
 public class Autos {
   private final AutoFactory autoFactory;
@@ -16,7 +19,17 @@ public class Autos {
     this.drive = drive;
 
     autoFactory =
-        new AutoFactory(drive::getPose, drive::setPose, drive::followTrajectory, false, drive);
+        new AutoFactory(
+            drive::getPose,
+            drive::setPose,
+            drive::followTrajectory,
+            false,
+            drive,
+            this::logTrajectory);
+  }
+
+  private void logTrajectory(Trajectory<SwerveSample> traj, boolean finishing) {
+    Logger.recordOutput("Odometry/ChoreoTraj", traj.getPoses());
   }
 
   public Command odometryTest2Cmd() {
