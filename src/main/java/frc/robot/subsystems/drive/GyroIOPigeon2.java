0ab581c73bb3ab3efa1578d392c6c5e5770b6aed
liujip0 <liujip2020@gmail.com>
2025-12-23 11:17:12 -0500

    log raw gyro values

--- src/main/java/frc/robot/subsystems/drive/GyroIOPigeon2.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIOPigeon2.java
@@ -47,6 +47,7 @@ public class GyroIOPigeon2 implements GyroIO {
   @Override
   public void updateInputs(GyroIOInputs inputs) {
     inputs.connected = BaseStatusSignal.refreshAll(yaw, yawVelocity).equals(StatusCode.OK);
+    inputs.rawYawPosition = Rotation2d.fromDegrees(yaw.getValueAsDouble());
     inputs.yawPosition =
         Rotation2d.fromDegrees(yaw.getValueAsDouble() / DriveConstants.pigeonYawPositionFactor);
     inputs.yawVelocityRadPerSec = Units.degreesToRadians(yawVelocity.getValueAsDouble());
